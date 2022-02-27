673f2c052c4e6bd8039b00592bf006057cf58d3d
sakairi-nobuyuki <SAKAIRI.Nobuyuki@gmail.com>
2022-02-28 00:54:29 +0900

    conftest

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+import pytest
+
+@pytest.fixture(scope='session')
+def mock_exchange_rate_parameter():
+    parameter_dict = {
+        'source': 'yahoo_finance',
+        'target': 'USD',
+        'end': '2022/02/27 12:05',
+        'duration': '100'
+    }
+    return parameter_dict
