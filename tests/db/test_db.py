88dbb46126670dfe35a0b1fd4f211b24f26b67e0
sakairi-nobuyuki <SAKAIRI.Nobuyuki@gmail.com>
2022-05-18 01:30:09 +0900

    test db

---
+++ tests/db/test_db.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+
+import os
+from sqlalchemy.orm import sessionmaker
+import sqlalchemy
+import sqlalchemy.ext.declarative
+
+import configparser
+
+from db.settings import Base
+from db.models import InferenceResultsModel
+
+class TestDataBase:
+    def test_db_url(self):
+        config = configparser.ConfigParser()
+        config.read("../../db/alembic.ini")
+        
+        assert config["alembic"]["sqlalchemy.url"]
+        assert config["alembic"]["sqlalchemy.url"] == "sqlite:///time_series_inference.db"
+
+    def test_start_session(self):
+        if os.path.exists("time_series_inference.db"):
+            os.remove("time_series_inference.db")
+
+        config = configparser.ConfigParser()
+        config.read("../../db/alembic.ini")
+        url = config["alembic"]["sqlalchemy.url"]
+
+        engine = sqlalchemy.create_engine(url, echo=True)
+
+        Base.metadata.create_all(bind=engine)
+
+        type = "today"
+        name = "USDJPY"
+        value = 105.0
+        source = "frex"
+
+        res = InferenceResultsModel(id=1, type=type, name=name, value=value, source=source)
+        
+        ### insert
+        session = sessionmaker(bind=engine)()
+        res.print_model_by_id(res.id)
+        session.add(instance=res)
+        session.commit()
+        session.close()
+        
+        res = InferenceResultsModel(id=2, type=type, name=name, value=value, source=source)
+        res.print_model_by_id(res.id)
+        session.add(instance=res)
+        session.commit()
+        session.close()
+
+        ### select
+        session = sessionmaker(bind=engine)()
+        res_select_iter = session.query(InferenceResultsModel).all()
+        print("length: ", len(res_select_iter))
+        for res_select in res_select_iter:
+            print("in db: ", res_select.id, res_select.name, res_select.created_at)
+
+        assert res_select_iter[0].id == 1
+        assert res_select_iter[-1].id == 2
+        assert res_select_iter[-1].name == res_select_iter[-2].name
+
+        ### deinit
+        os.remove("time_series_inference.db")
